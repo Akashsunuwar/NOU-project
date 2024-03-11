16c6178702ceac0fe9ca7a9afc4bc343657a41c2
Akashsunuwar <97868234+Akashsunuwar@users.noreply.github.com>
2024-03-11 00:39:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =14453
+let accountEmail = "akash@gmail.com"
+var accountPassword = "12345"
+accountCity = "kathmandu"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "abc@gmail.com"
+accountPassword ="1111"
+accountCity = "preeti"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
